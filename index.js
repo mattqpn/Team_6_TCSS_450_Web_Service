df87060485aad258e318d131bebc0cecec9f5263
mattqpn <116048461+mattqpn@users.noreply.github.com>
2022-11-18 21:09:44 -0800

    added lab 5 changes

--- index.js
+++ index.js
@@ -19,9 +19,17 @@ app.use(express.json())
  */
 app.use(middleware.jsonErrorInBody)
 
-app.use('/auth', require('./routes/signin.js'))
+// app.use('/auth', require('./routes/signin.js'))
 
-app.use('/auth', require('./routes/register.js'))
+// app.use('/auth', require('./routes/register.js'))
+
+app.use('/phish', middleware.checkToken, require('./routes/phish.js'))
+
+app.use('/messages', middleware.checkToken, require('./routes/messages.js'))
+
+app.use('/chats', middleware.checkToken, require('./routes/chats.js'))
+
+app.use('/auth', middleware.checkToken, require('./routes/pushyregister.js'))
 
 /*
  * Return HTML for the / end point. 
