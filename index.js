09ee258a7c93b3f14455baefa99ac99e2ac45071
mattqpn <116048461+mattqpn@users.noreply.github.com>
2022-11-28 16:10:57 -0800

    version 2

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get("/", (request, response) => {
     response.writeHead(200, {'Content-Type': 'text/html'});
     for (i = 1; i < 7; i++) {
         //write a response to the client
-        response.write('<h' + i + ' style="color:blue">Hello World!</h' + i + '>'); 
+        response.write('<h' + i + ' style="color:blue">Hello World Version 2!</h' + i + '>'); 
     }
     response.end(); //end the response
 });
