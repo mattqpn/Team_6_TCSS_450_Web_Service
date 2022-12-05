e251610a17c6c8abad0c37de4371a0e410026b03
mattqpn <116048461+mattqpn@users.noreply.github.com>
2022-12-04 23:36:20 -0800

    Update password.js

--- routes/password.js
+++ routes/password.js
@@ -9,45 +9,80 @@ const pool = require('../utilities').pool
 const validation = require('../utilities').validation
 let isStringProvided = validation.isStringProvided
 
+const generateHash = require('../utilities').generateHash
+
 const router = express.Router()
 
-router.put('/', (request, response, next) => {
-    
+/**
+ * @api {post} /password Request to change password
+ */
+router.post('/', (request, response, next) => {
+
     //Retrieve data from query params
     const email = request.body.email
-    const password = request.body.password
+    const oldPassword = request.body.oldPassword
+    const newPassword = request.body.newPassword
 
     //Verify that the caller supplied all the parameters
     if(isStringProvided(email) 
-        && isStringProvided(password)) {
-        
-        const theQuery = "SELECT MemberId FROM MEMBERS WHERE Members.email=$1"
-        const values = [request.auth.email]
-        pool.query(theQuery, values)
-            .then(result => {
-                //stash the memberid into the request object to be used in the next function
-                request.memberid = result.rows[0].memberid
-                next()
-            })
-            .catch((error) => {
-                //log the error
-                console.log("Error on SELECT************************")
-                console.log(err)
-                console.log("************************")
-                console.log(err.stack)
-                response.status(400).send({
-                    message: err.detail
-                })
-            })
+        && isStringProvided(oldPassword)
+        && isStringProvided(newPassword)) {
+        next()
     } else {
         response.status(400).send({
             message: "Missing required information"
         })
     }
+}, (request, response, next) => {
+    // verify password
+    const theQuery = `SELECT saltedhash, salt, Credentials.memberid FROM Credentials
+                      INNER JOIN Members ON
+                      Credentials.memberid=Members.memberid 
+                      WHERE Members.email=$1`
+    const values = [email]
+    pool.query(theQuery, values)
+        .then(result => {
+            if (result.rowCount == 0) {
+                response.status(404).send({
+                    message: 'User not found' 
+                })
+                return
+            }
+
+            //Retrieve the salt used to create the salted-hash provided from the DB
+            let salt = result.rows[0].salt
+            
+            //Retrieve the salted-hash password provided from the DB
+            let storedSaltedHash = result.rows[0].saltedhash 
+
+            //Generate a hash based on the stored salt and the provided password
+            let providedSaltedHash = generateHash(oldPassword, salt)
+
+            //Did our salted hash match their salted hash?
+            if (storedSaltedHash === providedSaltedHash ) {
+                next()
+            } else {
+                //credentials dod not match
+                response.status(400).send({
+                    message: 'Credentials did not match' 
+                })
+            }
+        })
+        .catch((err) => {
+            //log the error
+            console.log("Error on SELECT************************")
+            console.log(err)
+            console.log("************************")
+            console.log(err.stack)
+            response.status(400).send({
+                message: err.detail
+            })
+        })
+
 }, (request, response) => {
     //We're storing salted hashes to make our application more secure
     let salt = generateSalt(32)
-    let salted_hash = generateHash(request.body.password, salt)
+    let salted_hash = generateHash(newPassword, salt)
 
     let theQuery = "UPDATE CREDENTIALS SET SaltedHash = $1, Salt = $2 WHERE MemberId = $3"
     let values = [salted_hash, salt]
