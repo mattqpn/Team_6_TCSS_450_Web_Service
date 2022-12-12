80cc03e77cb1b2d16539212ebc219b54a32e79f9
elibreyes <79386861+elibreyes@users.noreply.github.com>
2022-12-11 18:56:54 -0800

    trying to grab the user name of the last person

--- routes/chats.js
+++ routes/chats.js
@@ -35,11 +35,11 @@ router.get("/", (request, response) => {
     // let query = `SELECT CHATS.ChatId FROM CHATS INNER JOIN CHATMEMBERS ON CHATS.ChatId=CHATMEMBERS.ChatId 
     //             WHERE MemberId=$1`
   
-    let query = `SELECT CHATS.ChatId, CHATS.Name FROM CHATS INNER JOIN CHATMEMBERS ON CHATS.ChatId=CHATMEMBERS.ChatId 
-                WHERE MemberId=$1`
+    // let query = `SELECT CHATS.ChatId, CHATS.Name FROM CHATS INNER JOIN CHATMEMBERS ON CHATS.ChatId=CHATMEMBERS.ChatId 
+    //             WHERE MemberId=$1`
 
-    // let query = `SELECT CHATS.ChatId, CHATS.Name, MEMBERS.Username, FROM CHATS INNER JOIN CHATMEMBERS ON CHATS.ChatId=CHATMEMBERS.ChatId 
-    //             WHERE MemberId=$1 INNER JOIN MEMBERS on CHATMEMBERS.MemberId=Members.MemberId WHERE MemberId=$1`
+    let query = `SELECT CHATS.ChatId, CHATS.Name, MEMBERS.Username, FROM CHATS INNER JOIN CHATMEMBERS ON CHATS.ChatId=CHATMEMBERS.ChatId 
+                WHERE MemberId=$1 INNER JOIN MEMBERS on CHATMEMBERS.MemberId=Members.MemberId WHERE MemberId=$1`
 
     let values = [request.decoded.memberid]
     pool.query(query, values)
