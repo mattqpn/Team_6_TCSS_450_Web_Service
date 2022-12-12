e0fd9e3cd968cede1f5b1476c859046802be61b1
elibreyes <79386861+elibreyes@users.noreply.github.com>
2022-12-11 16:05:43 -0800

    edit get call to original

--- routes/chats.js
+++ routes/chats.js
@@ -32,8 +32,9 @@ let isStringProvided = validation.isStringProvided
  */ 
 router.get("/", (request, response) => {
     //Retrieve the members
-    let query = `SELECT CHATS.ChatId, CHATS.Name, CHATS. FROM CHATS INNER JOIN CHATMEMBERS ON CHATS.ChatId=CHATMEMBERS.ChatId 
+    let query = `SELECT CHATS.ChatId, CHATS.Name FROM CHATS INNER JOIN CHATMEMBERS ON CHATS.ChatId=CHATMEMBERS.ChatId 
                 WHERE MemberId=$1`  
+                
     let values = [request.decoded.memberid]
     pool.query(query, values)
         .then(result => {
